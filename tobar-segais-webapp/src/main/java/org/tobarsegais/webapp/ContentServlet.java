e75aae85d1ed4977bfacf8e0392f51ef33bd115d
Arnaud Héritier <aheritier@apache.org>
2011-11-03 12:42:43 +0100

    Apply the same ASL header everywhere Copyright to Stephen for now

--- tobar-segais-webapp/src/main/java/org/tobarsegais/webapp/ContentServlet.java
+++ tobar-segais-webapp/src/main/java/org/tobarsegais/webapp/ContentServlet.java
@@ -1,3 +1,19 @@
+/*
+ * Copyright 2011 Stephen Connolly
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package org.tobarsegais.webapp;
 
 import org.apache.commons.io.IOUtils;
@@ -15,13 +31,6 @@ import java.net.URLConnection;
 import java.util.jar.JarEntry;
 import java.util.jar.JarFile;
 
-/**
- * Created by IntelliJ IDEA.
- * User: stephenc
- * Date: 18/10/2011
- * Time: 09:35
- * To change this template use File | Settings | File Templates.
- */
 public class ContentServlet extends HttpServlet {
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
